cfb0637dd089a4807bad576d09c96f3e17cc43e0
AdrianaReis <dicareis@gmail.com>
2017-07-03 09:19:48 -0400

    v1

--- xcodeGit/ViewController.swift
+++ xcodeGit/ViewController.swift
@@ -13,12 +13,18 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        
+        print ("Je suis un exemple de GIT... C'est tout....")
+        
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
+    
+    
 
 
 }
